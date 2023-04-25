9336092867657c902bae9e61a3e2446657968c7a
cdad32 <striker2143@gmail.com>
2023-04-25 21:11:50 +0900

    초기 커밋.

---
+++ ProgrammersStudy.cpp
@@ -0,0 +1,39 @@
+﻿#include <string>
+#include <vector>
+#include <unordered_map>
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+}
+
+vector<int> solution(vector<string> name, vector<int> yearning, vector<vector<string>> photo) {
+    vector<int> answer;
+
+    unordered_map<string, int> nameYearningMap;
+    int size = name.size();
+
+    for (int i = 0; i < size; i++)
+    {
+        nameYearningMap[name[i]] = yearning[i];
+    }
+    
+    int colSize = photo.size();
+
+    for (int i = 0; i < colSize; i++)
+    {
+        int rowSum = 0;
+        int rowSize = photo[i].size();
+        for (int j = 0; j < rowSize; j++)
+        {
+            rowSum += nameYearningMap[photo[i][j]];
+        }
+        
+        answer.push_back(rowSum);
+    }
+
+    return answer;
+}
